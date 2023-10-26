bac3acdd6948fcc109f7ee7491396443d19c46f9
Navneet <navneetbodkhe@gmail.com>
2023-10-26 07:53:12 +0530

    Custom.js recovered

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clear_cache',
     'accounts',
     'vendor',
     'menu',
